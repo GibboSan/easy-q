2fe6cdc4c6781584147127548bced993a5ccfe6d
Gibbone <gabriele.sanguin.2@studenti.unipd.it>
2025-12-01 18:48:04 +0100

    added and saved my IBM service account

--- quantum_optimization/pipeline/backends.py
+++ quantum_optimization/pipeline/backends.py
@@ -19,16 +19,29 @@ def get_aer_from_backend(seed: int, backend: Any = None) -> AerSimulator:
 
 def get_real_backend(backend_name: str) -> BackendV2:
 
-    crn = "insert-crn"
-    key = "insert-key"
+    # crn = "insert-crn"
+    # key = "insert-key"
 
     service = QiskitRuntimeService(
-        channel="ibm_quantum_platform",
-        instance=crn,
-        token=key
+        # channel="ibm_quantum_platform",
+        # instance=crn,
+        # token=key
     )
 
     return service.backend(backend_name)
+
+# SAVE YOUR ACCESS CREDENTIALS for SPECIFIC INSTANCES
+# to run previously in the environment.
+
+# from qiskit_ibm_runtime import QiskitRuntimeService
+ 
+# QiskitRuntimeService.save_account(
+#   token="<your-api-key>", # Use the 44-character API_KEY you created and saved from the IBM Quantum Platform Home dashboard
+#   name="<account-name>", # Optional
+#   instance="<IBM Cloud CRN or instance name>", # Optional
+#   set_as_default=True, # Optional
+#   overwrite=True, # Optional
+# )
 
 
 
