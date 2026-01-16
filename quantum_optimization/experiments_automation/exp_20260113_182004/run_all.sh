b78d42d2032c92396087add36f76a894d8330ccc
gibbo <gabriele.sanguin@gmail.com>
2026-01-16 18:19:32 +0100

    chore: deleting some experiments outputs and update of yaml files

--- quantum_optimization/experiments_automation/exp_20260113_182004/run_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-mkdir -p N_nodes6_N_layers1_out && python3 ../../run_performance.py N_nodes6_N_layers1/N_nodes6_N_layers1.yaml > N_nodes6_N_layers1_out/output.log
-mkdir -p N_nodes6_N_layers2_out && python3 ../../run_performance.py N_nodes6_N_layers2/N_nodes6_N_layers2.yaml > N_nodes6_N_layers2_out/output.log
-mkdir -p N_nodes9_N_layers1_out && python3 ../../run_performance.py N_nodes9_N_layers1/N_nodes9_N_layers1.yaml > N_nodes9_N_layers1_out/output.log
-mkdir -p N_nodes9_N_layers2_out && python3 ../../run_performance.py N_nodes9_N_layers2/N_nodes9_N_layers2.yaml > N_nodes9_N_layers2_out/output.log
-mkdir -p N_nodes12_N_layers1_out && python3 ../../run_performance.py N_nodes12_N_layers1/N_nodes12_N_layers1.yaml > N_nodes12_N_layers1_out/output.log
-mkdir -p N_nodes12_N_layers2_out && python3 ../../run_performance.py N_nodes12_N_layers2/N_nodes12_N_layers2.yaml > N_nodes12_N_layers2_out/output.log
